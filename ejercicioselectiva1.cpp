c8b5822a5ca926a8cf5e2dda1caaca59bb77fe66
maricielo <cielotapia198@gmail.com>
2025-05-31 18:44:05 -0500

    Agregando validación individual por nota usando bucles do-while para evitar valores negativos

--- ejercicioselectiva1.cpp
+++ ejercicioselectiva1.cpp
@@ -5,26 +5,40 @@ int main()
 {
     double N1, N2, N3, P;
     cout << "BIENVENIDO A MI PROGRAMA" <<endl;
-
-    cout <<"Ingrese la primera nota:" <<endl;
-    cin >> N1;
-
-    cout <<"Ingrese la segunda nota:"<<endl;
-    cin >> N2;
-
-    cout <<"Ingrese la tercera nota:"<<endl;
-    cin >> N3;
-    if (N1 < 0 || N2 < 0 || N3 < 0 ){
-		cout << "ingrese una nota valida, esta no debe ser negativa." << endl;
+	
+	do {
+    	cout << "Ingrese la primera nota:" << endl;
+        cin >> N1;
+        if (N1 < 0) {
+            cout << "Nota invalida. Ingrese una nota mayor o igual a 0" << endl;
+        }
+    } while (N1 < 0);
+    
+	do{
+		cout << "Ingrese la segunda nota:" << endl;
+        cin >> N2;
+        if (N2 < 0) {
+            cout << "Nota invalida. Ingrese una nota mayor o igual a 0" << endl;
+        }
+    } while (N2 < 0);
+		
+	do {
+		cout << "Ingrese la tercera nota: " << endl;
+        cin >> N3;
+        if (N3 < 0) {
+            cout << "Nota invalida. Ingrese una nota mayor o igual a 0" << endl;
+        }
+    } while (N3 < 0);
+	
+	P = (N1 + N2 + N3) / 3;
+	
+	cout <<"Tu promedio es: "<< P << endl;
+	if (P >= 70) {
+		cout <<"Alumno APROBADO." << endl;
 	} else {
-		P = (N1 + N2 + N3) / 3;
-		cout <<"Tu promedio es: "<< P << endl;
-		if (P >= 70) {
-			cout <<"Alumno APROBADO." << endl;
-		} else {
-        	cout <<"Alumno REPROBADO."<< endl;
-		}
+		cout <<"Alumno REPROBADO."<< endl;
 	}
+	
 	cout << "GRACIAS POR USAR MI PROGRAMA <3" << endl;
 
     return 0;
