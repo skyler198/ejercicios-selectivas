3d8f9c90bdd396d4e1f6a944a576b5b45ece0eae
maricielo <cielotapia198@gmail.com>
2025-05-30 22:27:08 -0500

    Mejorando la bienvenida y agregando una despedida para el usuario, agregando la condicional if else para validar que los datos ingresados no sean negativos y avisarle al usuario que debe ingresar una nota valida

--- ejercicioselectiva1.cpp
+++ ejercicioselectiva1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     double N1, N2, N3, P;
-    cout << "bienvenido a mi programa" <<endl;
+    cout << "BIENVENIDO A MI PROGRAMA" <<endl;
 
     cout <<"Ingrese la primera nota:" <<endl;
     cin >> N1;
@@ -14,16 +14,18 @@ int main()
 
     cout <<"Ingrese la tercera nota:"<<endl;
     cin >> N3;
-
-    P = (N1 + N2 + N3) / 3;
-
-    cout <<"Tu promedio es: "<< P << endl;
-
-    if (P >= 70) {
-        cout <<"Alumno APROBADO." << endl;
-    } else {
-        cout <<"Alumno REPROBADO."<< endl;
-    }
+    if (N1 < 0 || N2 < 0 || N3 < 0 ){
+		cout << "ingrese una nota valida, esta no debe ser negativa." << endl;
+	} else {
+		P = (N1 + N2 + N3) / 3;
+		cout <<"Tu promedio es: "<< P << endl;
+		if (P >= 70) {
+			cout <<"Alumno APROBADO." << endl;
+		} else {
+        	cout <<"Alumno REPROBADO."<< endl;
+		}
+	}
+	cout << "GRACIAS POR USAR MI PROGRAMA <3" << endl;
 
     return 0;
 }
